88fafe4aa15475560a991213f87b89e4a0113052
Robert Parsons <parson48@ad.nmc.edu>
2019-10-30 15:08:34 -0400

    Add project files.

---
+++ HELLOOOOOOOOOOOONMC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HELLOOOOOOOOOOOONMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello NMC.");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
